becb755651f9fe00f927b355d159d0815338e9a6
3LOYSMILE <mikle.r2006@gmail.com>
2025-02-20 20:18:28 +0200

    yeap

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Це підвал сайту</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import styles from "./Main.css";
+
+function Main() {
+  return (
+    <main className="main">
+      <h2>Головний контент</h2>
+      <button className="main-button">Натисни мене</button>
+    </main>
+  );
+}
+
+export default Main;
